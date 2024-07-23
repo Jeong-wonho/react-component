6c4659ac4d0cf0b7dcf4bb609cdff7bc9c08acab
정원호 <whjeong@jeong-wonhoui-noteubug.local>
2024-07-23 15:50:52 +0900

    table component 추가

--- src/components/common/table/DemoTable.jsx
+++ src/components/common/table/DemoTable.jsx
@@ -5,8 +5,8 @@ import CommonTable from './CommonTable'
 const DemoTable = () => {
   // const [columns, setColumns] = useState([""]);
   // setColumns(["상태적 목표", "기대결과 및 목표" , "담당부서", "1차 실무자", "2차 실무자", "목표치", "결과치", "달성률"]);
-  let columnName = ["상태적 목표", "기대결과 및 목표" , "담당부서", "1차 실무자", "2차 실무자", "목표치", "결과치", "달성률"];
-  let size = [100, 200, 150, 100, 100, 100, 100, 50];
+  let columnName = ["userId", "username", "email", "password", "registeredAt"];
+  let size = [100, 200, 150, 100, 100];
   
   return (
     <CommonTable columnName={columnName} size={size}/>
